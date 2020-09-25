0adeda31004d1f4b77491edf609ce7cca33f8242
Angel Cortez <slazen46@gmail.com>
2020-09-25 12:00:17 +0000

    Fourth commit

--- GitHomework.java
+++ GitHomework.java
@@ -1,6 +1,6 @@
 public class GitHomework{
 	public static void main(String[] args){
-		System.out.println("I am taking CS407 because I need the class for my major");
+		System.out.println("My favorite food is pasta");
 	}
 }
 
